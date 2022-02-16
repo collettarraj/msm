42b18007707009c37acb8315c40f615c96b9f15e
anonymous anonymous <collett.arraj@kellogg.northwestern.edu>
2022-02-16 20:46:45 +0000

    Generate graphiti resources for actors

---
+++ app/resources/actor_resource.rb
@@ -0,0 +1,14 @@
+class ActorResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :name, :string
+  attribute :dob, :date
+  attribute :image, :string
+  attribute :bio, :string
+
+  # Direct associations
+
+  # Indirect associations
+
+end
