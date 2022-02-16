8641e149e7f6fcfddcbf964b9dd7fec6cbf1f92d
anonymous anonymous <collett.arraj@kellogg.northwestern.edu>
2022-02-16 20:46:53 +0000

    Generate graphiti resources for movies

---
+++ app/resources/movie_resource.rb
@@ -0,0 +1,15 @@
+class MovieResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :title, :string
+  attribute :year, :date
+  attribute :description, :string
+  attribute :duration, :integer
+  attribute :director_id, :integer
+
+  # Direct associations
+
+  # Indirect associations
+
+end
